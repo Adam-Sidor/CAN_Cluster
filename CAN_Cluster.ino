a9673f8cf212259d2ed440faf4030bda43d9d3ea
Adam Sidor <sidoadsi1@gmail.com>
2024-07-19 19:47:46 +0200

    Pierwszy dzialajacy odczyt

--- CAN_Cluster.ino
+++ CAN_Cluster.ino
@@ -1,29 +1,62 @@
 #include <CAN.h>
-#define lo8(x) (uint8_t)((x)&0xff)
+
+#define lo8(x) (uint8_t)((x) & 0xff)
 #define hi8(x) (uint8_t)(((x) >> 8) & 0xff)
-#define CS_PIN  10
+#define CS_PIN 10
 #define IRQ_PIN 2
+
 uint32_t timestamp100ms = 0;
 uint32_t timestamp200ms = 0;
 uint32_t timetoblinkers = 0;
-String inputString = "";
+
+bool stringComplete = false;
+String inputString = "",inputValue="0";
+
+int speed = 0;
 
 void setup()
 {
-  Serial.begin(115200);
-  inputString.reserve(200);
-  CAN.setPins(CS_PIN, IRQ_PIN);
-  while (!CAN.begin(100E3))
-  {
-    Serial.println("Connecting with CAN failed!");
-    delay(100);
-  }
-  Serial.println("Connecting with CAN succesful!");
-  timestamp100ms = millis();
-  timestamp200ms = millis();
-  timetoblinkers = millis();
+    Serial.begin(115200);
+    inputString.reserve(200);
+    CAN.setPins(CS_PIN, IRQ_PIN);
+    CAN.setSPIFrequency(1E6);
+    /*while (!CAN.begin(100E3))
+    {
+      Serial.println("Connecting with CAN failed!");
+      delay(100);
+    }
+    Serial.println("Connecting with CAN succesful!");*/
+    timestamp100ms = millis();
+    timestamp200ms = millis();
+    timetoblinkers = millis();
 }
 
-void loop(){
+void loop()
+{
+    if (stringComplete)
+    {
+        if(inputString.indexOf("SPEED")!=-1){
+            inputValue=inputString.substring(5,inputString.length());
+        }
+        speed=inputValue.toInt();
+        Serial.println(speed);
+        Serial.println(inputString);
+        inputString = "";
+        stringComplete = false;
+    }
+}
 
+// functions
+
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        char inChar = (char)Serial.read();
+        if (inChar == '\n')
+        {
+            stringComplete = true;
+        }else
+            inputString += inChar;
+    }
 }
